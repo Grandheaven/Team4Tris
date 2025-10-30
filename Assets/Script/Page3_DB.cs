82e5a0dcb888bac8bfb07f6b86360a1df8e4bc11
Winglight <ljw5170520@gmail.com>
2025-10-30 20:45:19 +0900

    10.30. Image Loader Test
    asdf

--- Assets/Script/Page3_DB.cs
+++ Assets/Script/Page3_DB.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 
 public class Page3_DB : MonoBehaviour
 {
+    // Connection details
     private string host = "deu.duraka.shop";
     private string port = "4264";
     private string sid = "xe";
@@ -21,8 +22,15 @@ public class Page3_DB : MonoBehaviour
     public TMP_InputField publisher;
     public TMP_InputField price;
     public TMP_InputField url;
+    public RawImage bookPhoto;
     public TMP_InputField isbn;
+    public RawImage isbnPhoto;
     public TMP_InputField description;
+
+    [Header("Pop-Up Object")]
+    public GameObject successUI;
+    public GameObject failUI;
+    public TMP_Text failText;
 
     public void DataBaseConnection()
     {
@@ -34,19 +42,41 @@ public class Page3_DB : MonoBehaviour
             OracleCommand command = new OracleCommand("INSERT_BOOK", connection);
             command.CommandType = CommandType.StoredProcedure;
 
-            command.Parameters.Add("B_TITLE", OracleDbType.NVarchar2).Value = title.text;
-            command.Parameters.Add("B_AUTHOR", OracleDbType.NVarchar2).Value = author.text;
-            command.Parameters.Add("B_PUBLISHER", OracleDbType.NVarchar2).Value = publisher.text;
-            command.Parameters.Add("B_PRICE", OracleDbType.Decimal).Value = price.text;
-            command.Parameters.Add("B_URL", OracleDbType.Varchar2).Value = url.text;
-            command.Parameters.Add("B_BOOKPHOTO", OracleDbType.Blob).Value = DBNull.Value;
-            command.Parameters.Add("B_ISBN", OracleDbType.Decimal).Value = isbn.text;
-            command.Parameters.Add("B_ISBNPHOTO", OracleDbType.Blob).Value = DBNull.Value;
-            command.Parameters.Add("B_DESCRIPTION", OracleDbType.NVarchar2).Value = description.text;
+            try
+            {
+                byte[] book_photo = null;
+                if (this.bookPhoto.texture != null)
+                {
+                    Texture2D texture = this.bookPhoto.texture as Texture2D;
+                    book_photo = texture.EncodeToPNG();
+                }
 
-            command.ExecuteNonQuery();
+                byte[] isbn_photo = null;
+                if (this.isbnPhoto.texture != null)
+                {
+                    Texture2D texture = this.isbnPhoto.texture as Texture2D;
+                    isbn_photo = texture.EncodeToPNG();
+                }
 
-            Debug.Log("Data inserted successfully.");
+                command.Parameters.Add("B_TITLE", OracleDbType.NVarchar2).Value = title.text;
+                command.Parameters.Add("B_AUTHOR", OracleDbType.NVarchar2).Value = author.text;
+                command.Parameters.Add("B_PUBLISHER", OracleDbType.NVarchar2).Value = publisher.text;
+                command.Parameters.Add("B_PRICE", OracleDbType.Decimal).Value = price.text;
+                command.Parameters.Add("B_URL", OracleDbType.Varchar2).Value = url.text;
+                command.Parameters.Add("B_BOOKPHOTO", OracleDbType.Blob).Value = (book_photo == null) ? DBNull.Value : book_photo;
+                command.Parameters.Add("B_ISBN", OracleDbType.Decimal).Value = isbn.text;
+                command.Parameters.Add("B_ISBNPHOTO", OracleDbType.Blob).Value = (isbn_photo == null) ? DBNull.Value : isbn_photo;
+                command.Parameters.Add("B_DESCRIPTION", OracleDbType.NVarchar2).Value = description.text;
+
+                command.ExecuteNonQuery();
+
+                successUI.SetActive(true);
+            }
+            catch (Exception ex) // 데이터 삽입 실패 시 예외 처리
+            {
+                failText.text = ex.Message;
+                failUI.SetActive(true);
+            }
         }
         catch (Exception ex) // DB 연결 실패 시 예외 처리
         {
@@ -59,10 +89,9 @@ public class Page3_DB : MonoBehaviour
         }
     }
 
-    /*
-    private string ReadString(object dbValue)
+    public void ClosePopUp()
     {
-        return (dbValue == DBNull.Value || dbValue == null) ? string.Empty : dbValue.ToString();
+        successUI.SetActive(false);
+        failUI.SetActive(false);
     }
-    */
 }
